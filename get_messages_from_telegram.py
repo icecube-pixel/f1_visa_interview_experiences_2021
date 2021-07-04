5302cb84a92c70bdacd67c8b162952d7532faf2f
icecube-pixel <nithinbharadwajd@outlook.com>
2021-07-04 20:28:54 +0530

    Added initial script and config file to read the passwords

---
+++ get_messages_from_telegram.py
@@ -0,0 +1,13 @@
+import configparser
+import json
+
+from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
+
+
+def main():
+    print("Inside main")
+
+
+if __name__ == "__main__":
+    main()
