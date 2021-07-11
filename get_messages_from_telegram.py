fcbd700ecad362c45efe8dd6498565ad7edc271e
icecube-pixel <nithinbharadwajd@outlook.com>
2021-07-11 14:23:33 +0530

    Added changes in the script and added requirements.txt

--- get_messages_from_telegram.py
+++ get_messages_from_telegram.py
@@ -1,12 +1,48 @@
 import configparser
-import json
+import pandas as pd
+import sys
+import logging
+from telethon.sync import TelegramClient
+from telethon import TelegramClient, events, sync
 
-from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
+
+# Initializing logging handler
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+stdout_handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    '[%(asctime)s] p%(process)s {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s', '%m-%d %H:%M:%S')
+
+stdout_handler.setFormatter(formatter)
+logger.addHandler(stdout_handler)
+
+
+# Reading and returning the config as dict
+def read_config():
+    logger.info("Reading config")
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    config_dict = dict(config['Telegram'])
+    return config_dict
 
 
 def main():
-    print("Inside main")
+    logging.info("Inside main")
+    config_dict = read_config()
+    name, api_id, api_hash, chat = config_dict.get(
+        'username'), config_dict.get('api_id'), config_dict.get('api_hash'), config_dict.get('chat')
+    messages_list = []
+    try:
+        with TelegramClient(name, api_id, api_hash) as client:
+            for message in client.iter_messages(chat):
+                messages_list.append({"sender_id": message.sender_id,
+                                      "message": message.text})
+
+            client.disconnect()
+    except Exception as e:
+        logger.error("Exception occurred ", exc_info=True)
+        logger.error(e)
+    pd.DataFrame(messages_list).to_csv("Messages_text.csv", index=False)
 
 
 if __name__ == "__main__":
